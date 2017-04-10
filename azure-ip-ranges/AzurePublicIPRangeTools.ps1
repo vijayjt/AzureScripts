e2a6b34f12aad0e664d7f2df66b1c87f8070ad6d
Vijay Thakorlal <Vijay Thakorlal>
2017-04-10 21:57:34 +0100

    Added PowerShell module for getting the Azure Public IP address XML and manipulatting it.

---
+++ azure-ip-ranges/AzurePublicIPRangeTools.ps1
@@ -0,0 +1,233 @@
+﻿Function Get-AzurePublicIPRangesXMLFile
+{
+<#
+    .SYNOPSIS
+        This function downloads the Azure IP ranges XML file to the current directory or a specified path.
+    .DESCRIPTION
+        This function downloads the Azure IP ranges XML file to the current directory or a specified path.
+    .PARAMETER AzureIPRangeURL
+        An optional parameter that is the URL to the Azure IP range XML file download page.
+    .PARAMETER DestinationPath
+        The locaiton on the local filesystem where the Azure IP range XML file is to be downloaded.
+    .EXAMPLE
+        Get-AzurePublicIPRangesXMLFile -DestinationPath C:\AzureIPXMLFiles\
+        Get-AzurePublicIPRangesXMLFile -DestinationPath C:\AzureIPXMLFiles\ -AzureIPRangeURL 'https://www.microsoft.com/en-gb/download/confirmation.aspx?id=41653'
+    .NOTES
+#>
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$false,HelpMessage='Enter the URL for the Azure IP range XML file')]
+        [String]$AzureIPRangeURL = 'https://www.microsoft.com/en-gb/download/confirmation.aspx?id=41653',
+    [Parameter(Mandatory=$true,HelpMessage='Enter the path where the XML file should be written')]
+        [ValidateScript({ Test-Path $_ })]
+        [String]$DestinationPath
+)
+    If( $null -eq $AzureIPRangeURL ) { $AzureIPRangeURL = 'https://www.microsoft.com/en-gb/download/confirmation.aspx?id=41653' }
+    $AzureIPRangePage = (Invoke-WebRequest -Uri $AzureIPRangeURL )
+    $AzureIPRangeXMLFileURL = ($AzureIPRangePage.Links |  Where-Object { $_.href -like "*PublicIP*xml" }).href
+
+    Invoke-WebRequest -Uri $AzureIPRangeXMLFileURL -OutFile "$DestinationPath\AzurePublicIPRanges.xml"
+
+}#EndFunction Get-AzureIPRangesXMLFile
+
+
+Function Get-AzureRegionPublicIPAddressList
+{
+<#
+    .SYNOPSIS
+        The function lists the public IP addresses in a particular Azure region
+    .DESCRIPTION
+        The function lists the public IP addresses in a particular Azure region.
+    .PARAMETER Region
+        The Azure region for which you want to retrieve public IP addresses.
+    .PARAMETER AzureIPRangeXMLFile
+        The XML file containing the Azure IP ranges.
+    .PARAMETER OutputAsNSGAllowRuleFormat
+        This will result in the IP addresses to be output as outbound allow Network Security Group (NSG) rules in CSV form.
+        Placeholder values will be used for the rule priority (RulePriority), subnet address prefix (SubnetAddressPrefix).
+        For each IP address/range two rules will be listed one for HTTP and another for HTTPS.
+    .EXAMPLE
+        Get-AzureRegionPublicIPAddressList -Region 'europenorth' -AzureIPRangeXMLFile C:\AzurePublicIPs.xml
+        Get-AzureRegionPublicIPAddressList -Region 'europenorth' -AzureIPRangeXMLFile C:\AzurePublicIPs.xml -OutputAsNSGAllowRuleFormat
+#>
+ [CmdletBinding()]
+ Param(
+    [Parameter(Mandatory=$true,HelpMessage='Enter the region.')]
+    [String]$Region,
+    [Parameter(Mandatory=$true,HelpMessage='Enter the path to the Azure IP ranges XML file.')]
+    [String]$AzureIPRangeXMLFile,
+    [Parameter(ParameterSetName='NSGFormat',Mandatory=$false,HelpMessage='This switch causes the IP addresses to be listed as allow NSG rules.')]
+    [Switch]$OutputAsNSGAllowRuleFormat,    
+    [Parameter(ParameterSetName='NSGFormat',Mandatory=$true,HelpMessage='This switch causes the IP addresses to be listed as allow NSG rules.')]
+    [String]$NSGRuleNamePrefix = 'Allow-AzurePlatformIP-',
+    [Parameter(ParameterSetName='IPSecurityFormat',Mandatory=$false,HelpMessage='This switch causes the IP addresses to be listed as allow IIS Ip Security rules XML.')]
+    [Switch]$OutputAsIisIpSecurityXMLFormat
+ )
+
+$IpSecuirtyXml = [xml] @'
+<system.webServer>
+    <security>
+    <!--Unlisted IP addresses are denied access-->
+    <ipSecurity allowUnlisted="false">
+        <!--The following IP addresses are granted access-->
+        <add allowed="true" ipAddress="0.0.0.0" subnetMask="255.255.255.255" />
+    </ipSecurity>
+    </security>
+</system.webServer>
+'@
+
+$IpSecuirtyXml.'system.webServer'.security.ipSecurity.InnerXml = $null 
+
+    If( $OutputAsNSGAllowRuleFormat )
+    {
+        Write-Output "RuleName,Priority,Action,Direction,SourceAddressPrefix,SourcePortRange,DestinationAddressPrefix,DestinationPortRange,Protocol,Description"
+    }
+
+    $AzureIPRanges = [xml] (Get-Content $AzureIPRangeXMLFile)
+    #Check that the supplied region matches one in the file
+    If( $AzureIPRanges.AzurePublicIpAddresses.Region.Name -contains $Region)
+    {
+        $AzureIPRanges.AzurePublicIpAddresses.Region | ForEach-Object {
+            If($_.Name -eq $Region )
+            {
+                $RuleNumber = 1;
+                ForEach( $IPAddress in $_.IpRange )
+                {                                                                                  
+                    If( $OutputAsNSGAllowRuleFormat )
+                    {
+                        Write-Output "$($NSGRuleNamePrefix)-HTTPS-$RuleNumber-Outbound,RulePriority,Allow,Outbound,SubnetAddressPrefix,*,$($IpAddress.Subnet),443,TCP,Allow Azure VM Agent and Extension HTTPS traffic"
+                        $RuleNumber++
+                        Write-Output "$($NSGRuleNamePrefix)-HTTP-$RuleNumber-Outbound,RulePriority,Allow,Outbound,SubnetAddressPrefix,*,$($IpAddress.Subnet),80,TCP,Allow Azure VM Agent and Extension HTTP traffic"
+                        $RuleNumber++
+                    }
+                    ElseIf( $OutputAsIisIpSecurityXMLFormat )
+                    {
+                        $IPAddress = ($IPAddress.Subnet -split '/')[0]
+                        $MaskLength = ($IPAddress.Subnet -split '/')[1]
+                        [IPAddress] $ip = 0
+                        $ip.Address = ([UInt32]::MaxValue -1) -shl (32 - $MaskLength) -shr (32 - $MaskLength)                        
+                        $xmlElt = $IpSecuirtyXml.CreateElement("add")
+                        $xmlAtt = $IpSecuirtyXml.CreateAttribute("ipAddress")
+                        $xmlAtt.Value = $IPAddress.toString()
+                        [void] $xmlElt.Attributes.Append($xmlAtt)
+                        $xmlAtt = $IpSecuirtyXml.CreateAttribute("subnetMask")
+                        $xmlAtt.Value =  $ip.IPAddressToString.toString()
+                        [void] $xmlElt.Attributes.Append($xmlAtt)
+                        [void] $IpSecuirtyXml.'system.webServer'.security.ipSecurity.AppendChild($xmlElt)
+                    }
+                    Else
+                    {
+                        $IPAddress
+                    }
+                }
+            }        
+        }
+    }
+    Else
+    {
+        Throw "The supplied region $Region is not in the list of regions in the Azure IP Range XML file ($($AzIPs.AzurePublicIpAddresses.Region.Name -join ","))"
+    }
+
+    If( $OutputAsIisIpSecurityXMLFormat )
+    {
+      return $IpSecuirtyXml.OuterXml
+    }
+}#EndFunction Get-AzureRegionPublicIPAddressList
+
+
+Function Get-AzurePubicIpXMLRegionList
+{
+<#
+    .SYNOPSIS
+        The function takes the Azure Public IP address XML file and returns an array of region names
+    .DESCRIPTION
+        The function takes the Azure Public IP address XML file and returns an array of region names
+    .PARAMETER AzureIPRangeXMLFile
+        The XML file containing the Azure IP ranges.
+    .EXAMPLE
+        Get-AzureRegionPublicIPAddressList -AzureIPRangeXMLFile C:\AzurePublicIPs.xml
+#>
+ [CmdletBinding()]
+ [OutputType([object[]])]
+ Param(
+    [Parameter(Mandatory=$true,HelpMessage='Enter the path to the Azure IP ranges XML file.')]
+    [String]$AzureIPRangeXMLFile
+)
+    $AzureIPRanges = [xml] (Get-Content $AzureIPRangeXMLFile)    
+    return $AzureIPRanges.AzurePublicIpAddresses.Region.Name
+}#EndFuncion Function Get-AzurePubicIpXMLRegionList
+
+
+Function Test-IPIsInNetwork
+{
+<#
+    .SYNOPSIS
+        This function checks if an IP address falls within a particular IP range
+    .DESCRIPTION
+        This function checks if an IP address falls within a particular IP range (in CIDR format)
+    .PARAMETER IpAddress
+        The IP Address
+    .PARAMETER IpRangeInCidrFormat
+        The IP Address range specified in CIDR format e.g. 192.168.0.0/24
+    .PARAMETER
+        Test-IPIsInNetwork -IpAddress ‘192.168.250.10’ -IpRangeInCidrFormat ‘192.168.240.0/20’ 
+
+    .EXAMPLE
+#>
+    [CmdletBinding()]
+    [OutputType([Boolean])]
+    Param(
+        [Parameter(Mandatory=$true,HelpMessage='Enter the IP address')]
+            [string] $IpAddress,
+        [Parameter(Mandatory=$true,HelpMessage='The IP range in cidr format')]
+            [string] $IpRangeInCidrFormat
+    )
+
+    $network, [int]$subnetlen = $IpRangeInCidrFormat.Split('/')
+    $a = [uint32[]]$network.split('.')
+    [uint32] $unetwork = ($a[0] -shl 24) + ($a[1] -shl 16) + ($a[2] -shl 8) + $a[3]
+
+    $mask = (-bnot [uint32]0) -shl (32 - $subnetlen)
+
+    $a = [uint32[]]$IpAddress.split('.')
+    [uint32] $uip = ($a[0] -shl 24) + ($a[1] -shl 16) + ($a[2] -shl 8) + $a[3]
+
+    $unetwork -eq ($mask -band $uip)
+}#EndFunction Test-IPIsInNetwork
+
+
+Function Test-IPIsInAzureRegion
+{
+ [CmdletBinding()]
+ Param(
+    [Parameter(Mandatory=$true,HelpMessage='Enter the region.')]
+    [String]$Region,
+    [Parameter(Mandatory=$true,HelpMessage='Enter the path to the Azure IP ranges XML file.')]
+    [String]$AzureIPRangeXMLFile,
+    [Parameter(Mandatory=$true,HelpMessage='Enter the IP address')]
+    [string] $IpAddress 
+)
+    $IpIsInRegion = $false
+    $AzureIPRanges = [xml] (Get-Content $AzureIPRangeXMLFile)
+    #Check that the supplied region matches one in the file
+    If( $AzureIPRanges.AzurePublicIpAddresses.Region.Name -contains $Region)
+    {
+        $AzureIPRanges.AzurePublicIpAddresses.Region | ForEach-Object {
+            If($_.Name -eq $Region )
+            {
+                $RuleNumber = 1;
+                ForEach( $IPAddressRange in $_.IpRange )
+                {
+                    If( (Test-IPIsInNetwork -IpAddress $IpAddress -IpRangeInCidrFormat $IPAddressRange.Subnet) -eq $true )
+                    {
+                        $IpIsInRegion = $true
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return $IpIsInRegion
+}#Function Test-IPIsInAzureRegion
+
+Export-ModuleMember -Function Get-*
---
+++ rbac/Set-xAzureRoleAssignments.ps1
@@ -0,0 +1,234 @@
+﻿<#
+
+.SYNOPSIS
+    Applies a set of AD group and Azure RBAC roles to a subscription
+
+.DESCRIPTION
+    Applies a set of AD group and Azure RBAC roles to a subscription
+             
+.PARAMETER RoleAssigmentsCSVFile
+    A CSV file containing AD groups or users and the corresponding Azure RBAC role to be assigned.
+    
+@"
+ADObjectDisplayNameOrUPN,ADObjectType,RoleDefinition,ScopeLevel,TargetScope
+bob.smith@acme.com,User,Reader,subscription,
+MyADGroup1,Group,Owner,subscription,
+MyADGroup2,Group,Contributor,resourcegroup,RG-Test
+"@ | out-file example-role-assingments.csv
+
+    The ADObjectDisplayNameOrUPN field should contain the UPN if it's a user account and the display name if it is an AD group.
+    The ScopeLeveL field currently only supports:
+        - subscription: meaning the role will be scoped to the subscription specified by the SubscriptionName parameter or;
+        - resourcegroup: meaning the role will be scoped to a particular resouce group or set of resource groups
+
+    The TargetScope is left blank if the role is to be applied at the subscription level. If it the ScopeLevel is resourcegroup then:
+        - For single resource groups enter the resource group name
+        - For multiple resource groups enter a comma separated and quoted list of resource groups e.g. "rg1,rg2,rg3"
+
+.PARAMETER SubscriptionName
+    The name of the subscription where the role assignments are to be made.
+
+
+.EXAMPLE
+    
+    .\Set-AAMAzureRoleAssignments.ps1 -RoleAssigmentsCSVFile C:\example-role-assingments.csv -SubscriptionName TestSubscription
+    
+.NOTES
+    To Do:
+        1. Add errror checking so that if the role assignment already exists we do not attempt to duplicate it (as it will fail)
+    
+#>
+[Cmdletbinding()]
+Param(
+    [Parameter(Mandatory=$true,HelpMessage='The CSV file containing the role assignments')]
+        [ValidateScript({ Test-Path $_ })]
+        [String]$RoleAssigmentsCSVFile,
+    [Parameter(Mandatory=$true,HelpMessage='The name of the subscription where the role assignments are to be applied')]
+        [String]$SubscriptionName
+)
+
+#region --- MODULE IMPORTS ---
+
+#endregion
+
+
+#region --- VARIABLES ---
+$RoleAssigmentsCSVFileName = Split-Path -Path $RoleAssigmentsCSVFile -Resolve -Leaf
+$FileExtension = ($RoleAssigmentsCSVFileName -split '\.')[1]
+
+#endregion
+
+
+
+#region --- General Functions ---
+
+#endregion
+
+
+#region --- Azure Functions ---
+
+Function Test-IsAuthenticatedRmAPI
+{
+<# 
+   .SYNOPSIS 
+        This function checks if the user exeucting this script is authenticated against Azure AD
+   .DESCRIPTION 
+        This function checks if the user exeucting this script is authenticated against Azure AD
+   .EXAMPLE 
+        Test-IsAuthenticatedRmAPI
+   .NOTES
+        Requirements: Copy this module to any location found in $env:PSModulePath
+        This module depends on the Azure Resource Manager PowerShell modules
+   
+#>
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    Param()
+
+    try
+    {
+        Write-Debug 'Checking if you have been authenticated against Azure using Login-AzureRmAccount.'        
+        $AuthCheck1 = Get-AzureRmContext -ErrorAction Stop
+    }
+    catch
+    {
+        $ErrorIndex = $Error.Count - 1
+        If ( ($Error[$Error.Count-1].Exception | Out-String).Trim() -eq 'Run Login-AzureRmAccount to login.' ) 
+        { 
+            Write-Error 'Your Windows Azure credential in the Windows PowerShell session has expired. Please use Login-AzureRmAccount to login again.'            
+        }
+        else
+        {
+            Write-Error "An error occurred please try again. Displaying last `$Error variable contents: $($Error[$ErrorIndex-1])"
+        }
+        return $false 
+    }
+
+    $UserID = ($AuthCheck1 | Select-Object Account).Account
+    Write-Debug "Authenticated as $UserID (ARM Mode)"
+    return $true
+
+}#EndFunction Test-IsAuthenticatedRMAPI
+
+#endregion
+
+#region --- MAIN PROGRAM ---
+
+
+Write-Output "Validating that the role assignments file is a CSV file"
+
+# Check the file has a .csv extension
+If( $FileExtension.ToLower() -ne "csv" )
+{
+   throw "Error, the provided role assignments file $($RoleAssigmentsCSVFile) does not have a .csv extension. Please verify this is a valid CSV file."
+}
+
+# Check if we can pipe the file contents to ConvertFrom-Csv without error
+Try
+{
+    $ConvertedCSV = Get-Content $RoleAssigmentsCSVFile | ConvertFrom-Csv
+    
+}
+Catch 
+{
+    throw "Error, unable to parse the provided role assignments file $($RoleAssigmentsCSVFile) as CSV. Please verify this is a valid CSV file."
+}
+
+# Check if the CSV file has the expected properties
+$ExpectedProperties = @('ADObjectDisplayNameOrUPN','ADObjectType','RoleDefinition','ScopeLevel','TargetScope')
+
+ForEach ( $Property in ($ConvertedCSV | Get-Member -MemberType NoteProperty).Name )
+{
+    If( $ExpectedProperties -notcontains $Property )
+    {
+        throw "Error, the provided role assignments file $($RoleAssigmentsCSVFile) does not contain the expected fields"
+    }
+}
+
+
+Write-Output 'Checking if you are authenticated against the ARM API.'
+Write-Output ''
+
+If ( Test-IsAuthenticatedRmAPI )
+{
+    # Only proceed if the user has access to the subscription
+    If( Test-AzureSubscriptionExist $SubscriptionName )
+    {
+        #Change the current subscription
+        $SubscriptionContext = Set-AzureRmContext -SubscriptionName $SubscriptionName
+        ForEach( $Line in $ConvertedCSV )
+        {
+
+            # Determine scope
+            If( $Line.ScopeLevel -eq "subscription" )
+            {
+                $Scopes = "/subscriptions/{0}" -f $SubscriptionContext.Subscription.SubscriptionId
+            }
+            ElseIf( $Line.ScopeLevel -eq "resourcegroup" )
+            {
+                $ResourceGroups = $Line.TargetScope -split ','
+                If( $ResourceGroups.count -gt 1 )
+                {
+                    $Scopes = @()
+                    ForEach( $ResourceGroup in $ResourceGroups)
+                    {
+                        $Scopes += "/subscriptions/{0}/resourceGroups/{1}" -f $SubscriptionContext.Subscription.SubscriptionId,$ResourceGroup
+                    }
+                }            
+            }
+
+
+            #Check the user/group exists in Azure AD
+            If( $Line.ADObjectType -eq "User" -and (Test-AzureADUserExist -Username $Line.ADObjectDisplayNameOrUPN)  )
+            {
+                #Check the RoleDefinition exists in the subscription
+                If( Test-AzureRmRoleDefinitionExist -RoleDefinitionName $Line.RoleDefinition )
+                {
+                    ForEach($ScopeToApply in $Scopes)
+                    {
+                        $UserObject = Get-AzureRmADUser -UserPrincipalName $Line.ADObjectDisplayNameOrUPN
+                        Write-Output "Granting role $($Line.RoleDefinition) to user $($UserObject.DisplayName) at the scope $ScopeToApply"
+                        New-AzureRmRoleAssignment -ObjectId $UserObject.Id.Guid -RoleDefinitionName $Line.RoleDefinition -Scope $ScopeToApply
+                    }
+                }
+                Else
+                {
+                    Write-Error "Unable to grant role $($Line.RoleDefinition) to user $($UserObject.DisplayName) at the scope $ScopeToApply - the role definition does not exist in this subscription."
+                }
+            }
+            ElseIf( $Line.ADObjectType -eq "Group" -and (Test-AzureADGroupExist -ADGroup $Line.ADObjectDisplayNameOrUPN) )
+            {
+                #Check the RoleDefinition exists in the subscription
+                If( Test-AzureRmRoleDefinitionExist -RoleDefinitionName $Line.RoleDefinition )
+                {
+                    ForEach($ScopeToApply in $Scopes)
+                    {
+                        Write-Output "Granting role $($Line.RoleDefinition) to AD Group $($Line.ADObjectDisplayNameOrUPN) at the scope $ScopeToApply"
+                        $ADSecurityGroupObject = Get-AzureRmADGroup -SearchString $Line.ADObjectDisplayNameOrUPN 
+                        New-AzureRmRoleAssignment -ObjectId $ADSecurityGroupObject.Id.Guid -RoleDefinitionName $Line.RoleDefinition -Scope $ScopeToApply
+                    }
+                }            
+                Else
+                {
+                    Write-Error "Unable to grant role $($Line.RoleDefinition) to user $($UserObject.DisplayName) at the scope $ScopeToApply - the role definition does not exist in this subscription."
+                }
+            }
+            Else
+            {
+                Write-Error "The user/group $($Line.ADObjectDisplayNameOrUPN) does not exist in Azure AD"
+            }        
+        }
+    }
+    Else 
+    {
+        Throw "Error, the provided subscription $SubscriptionName does not exist or you do not have permissions to the subscription, exiting the script"
+    }
+
+}
+Else
+{
+    Throw 'Error: You are not authenticated, unable to continue.'
+}
+
+
+#endregion
