9b9265a0a79326bdab83a66c9ea9f014336a8b42
vijayjt <vijayjt@gmail.com>
2017-05-21 12:15:14 +0100

    Updated the AzurePublicIPRangeTools.ps1 to fix some bugs in the function to download Azure IPs and added additional output formats for Azure IPs.

--- azure-ip-ranges/AzurePublicIPRangeTools.ps1
+++ azure-ip-ranges/AzurePublicIPRangeTools.ps1
@@ -1,4 +1,5 @@
-﻿Function Get-AzurePublicIPRangesXMLFile
+﻿
+Function Get-AzurePublicIPRangesXMLFile
 {
 <#
     .SYNOPSIS
@@ -23,13 +24,74 @@ Param(
         [String]$DestinationPath
 )
     If( $null -eq $AzureIPRangeURL ) { $AzureIPRangeURL = 'https://www.microsoft.com/en-gb/download/confirmation.aspx?id=41653' }
-    $AzureIPRangePage = (Invoke-WebRequest -Uri $AzureIPRangeURL )
-    $AzureIPRangeXMLFileURL = ($AzureIPRangePage.Links |  Where-Object { $_.href -like "*PublicIP*xml" }).href
-
-    Invoke-WebRequest -Uri $AzureIPRangeXMLFileURL -OutFile "$DestinationPath\AzurePublicIPRanges.xml"
+    $AzureIPRangePage = (Invoke-WebRequest -UseBasicParsing -Uri $AzureIPRangeURL )
+    $AzureIPRangeXMLFileURL = (($AzureIPRangePage.Links |  Where-Object { $_.href -like "*PublicIP*xml" }).href)[0]
+    Write-Verbose "Azure IP Range XML File URL is $AzureIPRangeXMLFileURL"
+    Invoke-WebRequest -UseBasicParsing -Uri $AzureIPRangeXMLFileURL -OutFile "$DestinationPath\AzurePublicIPRanges.xml"
 
 }#EndFunction Get-AzureIPRangesXMLFile
 
+
+Function ConvertFrom-CidrNotation
+{
+<#
+    .SYNOPSIS
+        Converts a network in CIDR notation to a IP and Netmask notation
+    .DESCRIPTION
+        Converts a network in CIDR notation to a IP and Netmask notation
+    .PARAMETER NetworkCidr
+        The network provided in CIDR notation e.g. x.x.x.x/yy
+    .EXAMPLE
+        ConvertFrom-CidrNotation -NetworkCidr
+#>
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true,HelpMessage='Enter the network in cidr notation e.g. x.x.x.x/yy')]
+        [ValidateNotNullOrEmpty()]
+        [String]$NetworkCidr
+)
+
+	$CidrMapping = @{
+	'/32'='255.255.255.255';
+	'/31'='255.255.255.254';
+	'/30'='255.255.255.252';
+	'/29'='255.255.255.248';
+	'/28'='255.255.255.240';
+	'/27'='255.255.255.224';
+	'/26'='255.255.255.192';
+	'/25'='255.255.255.128';
+	'/24'='255.255.255.0';
+	'/23'='255.255.254.0';
+	'/22'='255.255.252.0';
+	'/21'='255.255.248.0';
+	'/20'='255.255.240.0';
+	'/19'='255.255.224.0';
+	'/18'='255.255.192.0';
+	'/17'='255.255.128.0';
+	'/16'='255.255.0.0';
+	'/15'='255.254.0.0';
+	'/14'='255.252.0.0';
+	'/13'='255.248.0.0';
+	'/12'='255.240.0.0';
+	'/11'='255.224.0.0';
+	'/10'='255.192.0.0';
+	'/9'='255.128.0.0';
+	'/8'='255.0.0.0';
+	'/7'='254.0.0.0';
+	'/6'='252.0.0.0';
+	'/5'='248.0.0.0';
+	'/4'='240.0.0.0';
+	'/3'='224.0.0.0';
+	'/2'='192.0.0.0';
+	'/1'='128.0.0.0';
+	'/0'='0.0.0.0';
+	}
+	$Network = $NetworkCidr.Split('/')[0]
+	$Cidr = ("/" + $NetworkCidr.Split('/')[1])
+	$Subnet = $CidrMapping.Item($Cidr)
+	return (New-Object PSObject -Property @{'Network'=$Network;'Subnet'=$Subnet})
+}#End Function ConvertFrom-CidrNotation
+ 
 
 Function Get-AzureRegionPublicIPAddressList
 {
@@ -43,12 +105,17 @@ Function Get-AzureRegionPublicIPAddressList
     .PARAMETER AzureIPRangeXMLFile
         The XML file containing the Azure IP ranges.
     .PARAMETER OutputAsNSGAllowRuleFormat
-        This will result in the IP addresses to be output as outbound allow Network Security Group (NSG) rules in CSV form.
+        This switch will result in the IP addresses to be output as outbound allow Network Security Group (NSG) rules in CSV form.
         Placeholder values will be used for the rule priority (RulePriority), subnet address prefix (SubnetAddressPrefix).
         For each IP address/range two rules will be listed one for HTTP and another for HTTPS.
+    .PARAMETER OutputAsIpSecurityXMLFormat
+        This switch will output the IP addresses as in IP Security XML format that is typically used in IIS IP Security restrictions.
+    .PARAMETER OutputAsCheckpointObjectGroupFormat
+        This switch will output the IP addresses in Checkpoint network object group format
     .EXAMPLE
-        Get-AzureRegionPublicIPAddressList -Region 'europenorth' -AzureIPRangeXMLFile C:\AzurePublicIPs.xml
-        Get-AzureRegionPublicIPAddressList -Region 'europenorth' -AzureIPRangeXMLFile C:\AzurePublicIPs.xml -OutputAsNSGAllowRuleFormat
+        Get-AzureRegionPublicIPAddressList -Region 'europenorth' -AzureIPRangeXMLFile C:\AzurePublicIPs.xml -OutputAsIisIpSecurityXMLFormat
+        Get-AzureRegionPublicIPAddressList -Region 'europenorth' -AzureIPRangeXMLFile C:\AzurePublicIPs.xml -OutputAsNSGAllowRuleFormat -NSGRuleNamePrefix 'Allow-AzurePlatformIP-'
+        Get-AzureRegionPublicIPAddressList -Region 'europenorth' -AzureIPRangeXMLFile C:\AzurePublicIPs.xml -OutputAsCheckpointObjectGroupFormat
 #>
  [CmdletBinding()]
  Param(
@@ -61,7 +128,9 @@ Function Get-AzureRegionPublicIPAddressList
     [Parameter(ParameterSetName='NSGFormat',Mandatory=$true,HelpMessage='This switch causes the IP addresses to be listed as allow NSG rules.')]
     [String]$NSGRuleNamePrefix = 'Allow-AzurePlatformIP-',
     [Parameter(ParameterSetName='IPSecurityFormat',Mandatory=$false,HelpMessage='This switch causes the IP addresses to be listed as allow IIS Ip Security rules XML.')]
-    [Switch]$OutputAsIisIpSecurityXMLFormat
+    [Switch]$OutputAsIpSecurityXMLFormat,
+    [Parameter(ParameterSetName='CFWNetworkObjGroupFormat',Mandatory=$false,HelpMessage='This switch causes the IP addresses to be output in Checkpoint firewall network object group format.')]
+    [Switch]$OutputAsCheckpointObjectGroupFormat
  )
 
 $IpSecuirtyXml = [xml] @'
@@ -83,6 +152,9 @@ $IpSecuirtyXml.'system.webServer'.security.ipSecurity.InnerXml = $null
         Write-Output "RuleName,Priority,Action,Direction,SourceAddressPrefix,SourcePortRange,DestinationAddressPrefix,DestinationPortRange,Protocol,Description"
     }
 
+    $NetworkName = $Network = $Subnet = $null
+	$NetworkList = @()
+    
     $AzureIPRanges = [xml] (Get-Content $AzureIPRangeXMLFile)
     #Check that the supplied region matches one in the file
     If( $AzureIPRanges.AzurePublicIpAddresses.Region.Name -contains $Region)
@@ -100,7 +172,7 @@ $IpSecuirtyXml.'system.webServer'.security.ipSecurity.InnerXml = $null
                         Write-Output "$($NSGRuleNamePrefix)-HTTP-$RuleNumber-Outbound,RulePriority,Allow,Outbound,SubnetAddressPrefix,*,$($IpAddress.Subnet),80,TCP,Allow Azure VM Agent and Extension HTTP traffic"
                         $RuleNumber++
                     }
-                    ElseIf( $OutputAsIisIpSecurityXMLFormat )
+                    ElseIf( $OutputAsIpSecurityXMLFormat )
                     {
                         $IPAddress = ($IPAddress.Subnet -split '/')[0]
                         $MaskLength = ($IPAddress.Subnet -split '/')[1]
@@ -115,6 +187,18 @@ $IpSecuirtyXml.'system.webServer'.security.ipSecurity.InnerXml = $null
                         [void] $xmlElt.Attributes.Append($xmlAtt)
                         [void] $IpSecuirtyXml.'system.webServer'.security.ipSecurity.AppendChild($xmlElt)
                     }
+                    ElseIf( $OutputAsCheckpointObjectGroupFormat )
+                    {
+                        $NetworkName = ("AzureNetwork$Region" + ("{0:D2}" -f $RuleNumber))
+                        $NetworkList += $NetworkName
+                        $Network = (ConvertFrom-CidrNotation $IPAddress.Subnet).Network
+	                    $Subnet = (ConvertFrom-CidrNotation $IPAddress.Subnet).Subnet
+	                    Write-Output "create network $($NetworkName)"
+	                    Write-Output "modify network_objects $($NetworkName) ipaddr $($Network)"
+	                    Write-Output "modify network_objects $($NetworkName) netmask $($Subnet)"
+	                    Write-Output "update network_objects $($NetworkName)"
+                        $RuleNumber++
+                    }
                     Else
                     {
                         $IPAddress
@@ -128,10 +212,20 @@ $IpSecuirtyXml.'system.webServer'.security.ipSecurity.InnerXml = $null
         Throw "The supplied region $Region is not in the list of regions in the Azure IP Range XML file ($($AzIPs.AzurePublicIpAddresses.Region.Name -join ","))"
     }
 
-    If( $OutputAsIisIpSecurityXMLFormat )
+    If( $OutputAsIpSecurityXMLFormat )
     {
       return $IpSecuirtyXml.OuterXml
     }
+
+    If( $OutputAsCheckpointObjectGroupFormat )
+    {
+        Write-Output "create network_object_group AzureNetworks$Region"
+        $NetworkList | % {
+	        Write-Output "addelement network_objects AzureNetworks$Region '' network_objects:$($_)" 
+        }
+        Write-Output "update network_objects AzureNetworks$Region"
+    }
+
 }#EndFunction Get-AzureRegionPublicIPAddressList
 
 
@@ -145,7 +239,7 @@ Function Get-AzurePubicIpXMLRegionList
     .PARAMETER AzureIPRangeXMLFile
         The XML file containing the Azure IP ranges.
     .EXAMPLE
-        Get-AzureRegionPublicIPAddressList -AzureIPRangeXMLFile C:\AzurePublicIPs.xml
+        Get-AzurePubicIpXMLRegionList -AzureIPRangeXMLFile C:\AzurePublicIPs.xml
 #>
  [CmdletBinding()]
  [OutputType([object[]])]
@@ -156,6 +250,7 @@ Function Get-AzurePubicIpXMLRegionList
     $AzureIPRanges = [xml] (Get-Content $AzureIPRangeXMLFile)    
     return $AzureIPRanges.AzurePublicIpAddresses.Region.Name
 }#EndFuncion Function Get-AzurePubicIpXMLRegionList
+
 
 
 Function Test-IPIsInNetwork
